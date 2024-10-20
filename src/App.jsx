cd3c09a33d0c4decf70f30a923dae7f4e6019bd4
code-walker-23 <chhabravinay549@gmail.com>
2024-10-20 17:09:22 +0530

    project-setup-routing-and-authentication-setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+const App = () => {
+  return (
+    <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4"></div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <PrivyProvider
+      appId="cm1x2dy38048b3iwyq71m2q4i"
+      config={{
+        appearance: {
+          theme: "light",
+        },
+      }}
+    >
+      <Router>
+        <App />
+      </Router>
+    </PrivyProvider>
+  </React.StrictMode>,
+);
