f38c035b7b5354875d60084f5103c7d754a802f4
code-walker-23 <chhabravinay549@gmail.com>
2024-10-25 22:37:19 +0530

    sidebar-completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,20 +2,40 @@ import React from "react";
 import { useState } from "react";
 import { navLinks } from "../constants";
 import { sun } from "../assets";
-
-const Icon = ({ style, name, imageUrl, isActive, disabled, handleClick }) => {
-  return (
-    <div
-      className={`h-[48px] w-[48px] rounded-[10px] ${isActive && isActive === name && "bg-[#2c2f32]"} flex items-center justify-center ${style}`}
-      onClick={handleClick}
-    >
-       
-    </div>
-  );
-};
+import { Link, useNavigate } from "react-router-dom";
+import { IconHeartHandshake } from "@tabler/icons-react";
+import Icon from "./Icon";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  return (
+    <div className="sticky top-5 flex h-[93vh] flex-col items-center justify-between">
+      <Link to={"/"}>
+        <div className="rounded-[10px] bg-[#2c2f32] p-2">
+          <IconHeartHandshake size={40} color="#1ec070" />
+        </div>
+      </Link>
+      <div className="mt-12 flex w-[76px] flex-1 flex-col items-center justify-between rounded-[20px] bg-[#1c1c24] py-4">
+        <div className="flex flex-col items-center justify-center gap-3">
+          {navLinks.map((item) => {
+            return (
+              <Icon
+                key={item.name}
+                {...item}
+                isActive={isActive}
+                handleClick={() => {
+                  setIsActive(item.name);
+                  navigate(item.link);
+                }}
+              />
+            );
+          })}
+        </div>
+        <Icon styles={"bg-[#1c1c24] shawdow-secondary"} imageUrl={sun} />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
