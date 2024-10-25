7acc0e8d07cc2e6cf77937b0038b32772f2d390f
code-walker-23 <chhabravinay549@gmail.com>
2024-10-25 22:38:16 +0530

    separate-component-to-render-icon

---
+++ src/components/Icon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Icon = ({ style, name, imageUrl, isActive, disabled, handleClick }) => {
+  return (
+    <div
+      className={`h-[48px] w-[48px] rounded-[10px] ${isActive && isActive === name && "bg-[#2c2f32]"} flex items-center justify-center ${style}`}
+      onClick={handleClick}
+    >
+      {!isActive ? (
+        <img src={imageUrl} alt="beat-cancer logo" className="h-6 w-6" />
+      ) : (
+        <img
+          src={imageUrl}
+          alt="beat-cancer logo"
+          className={`h-6 w-6 ${isActive !== name && "grayscale"}`}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Icon;
