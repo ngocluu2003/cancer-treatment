a9a5df901d9f9a4b2dcbd98dd098c29578ee942f
code-walker-23 <chhabravinay549@gmail.com>
2024-10-25 20:37:42 +0530

    building-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,18 @@
 import React from "react";
+import { useState } from "react";
+import { navLinks } from "../constants";
+import { sun } from "../assets";
+
+const Icon = ({ style, name, imageUrl, isActive, disabled, handleClick }) => {
+  return (
+    <div
+      className={`h-[48px] w-[48px] rounded-[10px] ${isActive && isActive === name && "bg-[#2c2f32]"} flex items-center justify-center ${style}`}
+      onClick={handleClick}
+    >
+       
+    </div>
+  );
+};
 
 const Sidebar = () => {
   return <div>Sidebar</div>;
