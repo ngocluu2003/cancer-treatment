ea47d307b6be5b954fbde07e9c6707317862caf7
code-walker-23 <chhabravinay549@gmail.com>
2024-10-26 22:34:06 +0530

    added-dark-light-mode

--- src/components/MetricsCard.jsx
+++ src/components/MetricsCard.jsx
@@ -1,30 +1,31 @@
 import { IconChevronRight } from "@tabler/icons-react";
 import React from "react";
+
 const MetricsCard = ({ title, subtitle, value, icon: Icon, onClick }) => {
   return (
-    <div className="flex flex-col rounded-xl border bg-white shadow-sm dark:border-neutral-800 dark:bg-[#13131a]">
+    <div className="flex flex-col rounded-xl border bg-white shadow-sm transition-colors duration-200 dark:border-neutral-800 dark:bg-[#13131a]">
       <div className="flex justify-between gap-x-3 p-4 md:p-5">
         <div>
-          <p className="text-xs uppercase tracking-wide text-neutral-500">
+          <p className="text-xs uppercase tracking-wide text-neutral-500 dark:text-neutral-400">
             {title}
           </p>
           <div className="mt-1 flex items-center gap-x-2">
-            <h3 className="text-xl font-medium text-neutral-200 sm:text-2xl">
+            <h3 className="text-xl font-medium text-neutral-800 sm:text-2xl dark:text-neutral-200">
               {value}
             </h3>
           </div>
         </div>
-      </div>
-      <div className="text-blue flex size-[46px] h-11 w-11 flex-shrink-0 items-center justify-center rounded-full bg-blue-600 text-blue-200 dark:bg-[#1c1c24]">
-        <Icon size={25} className="text-green-500" />
+        <div className="flex size-[46px] h-11 w-11 flex-shrink-0 items-center justify-center rounded-full bg-blue-600 text-blue-200 dark:bg-[#1c1c24] dark:text-neutral-400">
+          <Icon size={25} className="text-[#1ec070]" />
+        </div>
       </div>
       <a
-        className="inline-flex items-center justify-between rounded-b-xl border-t border-gray-200 px-4 py-3 text-sm text-gray-600 hover:bg-gray-50 md:px-5 dark:border-neutral-800 dark:text-neutral-400 dark:hover:bg-neutral-800"
+        className="inline-flex items-center justify-between rounded-b-xl border-t border-gray-200 px-4 py-3 text-sm text-gray-600 hover:bg-gray-50 md:px-5 dark:border-neutral-800 dark:text-neutral-400 dark:hover:bg-[#2c2f32]"
         href="#"
         onClick={onClick}
       >
         {subtitle}
-        <IconChevronRight />
+        <IconChevronRight className="text-neutral-500 dark:text-neutral-400" />
       </a>
     </div>
   );
