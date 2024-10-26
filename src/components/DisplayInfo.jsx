ed055d35331cea5f9d435c19479214a5d1623d5e
code-walker-23 <chhabravinay549@gmail.com>
2024-10-26 18:21:30 +0530

    building-metrics-card-

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DisplayInfo = () => {
+  const [metrics, setMetrics] = useState({
+    totalFolders: 0,
+    aiPersonalizedTreatment: 0,
+    totalScreenings: 0,
+    personalScreenings: 0,
+    pendningScreening: 0,
+    overdueScreenings: 0,
+  });
   return <div className="text-white">DisplayInfo</div>;
 };
 
---
+++ src/components/MetricsCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Icon from "./Icon";
+const MetricsCard = ({ title, subtitle, value, icon: Icon, onClick }) => {
+  return (
+    <div className="flex flex-col rounded-xl border bg-white shadow-sm dark:border-neutral-800 dark:bg-[#13131a]">
+      <div className="flex justify-between gap-x-3 p-4 md:p-5">
+        <div>
+          <p className="text-xs uppercase tracking-wide text-neutral-500">
+            {title}
+          </p>
+          <div className="mt-1 flex items-center gap-x-2">
+            <h3 className="text-xl font-medium text-neutral-200 sm:text-2xl">
+              {value}
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="text-blue flex size-[46px] h-11 w-11 flex-shrink-0 items-center justify-center rounded-full bg-blue-600 text-blue-200 dark:bg-[#1c1c24]">
+        <Icon size={25} />
+      </div>
+    </div>
+  );
+};
+
+export default MetricsCard;
