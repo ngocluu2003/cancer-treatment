215407ef5301b06fcd7005edaab7a5145edeb9c6
code-walker-23 <chhabravinay549@gmail.com>
2024-10-26 18:01:21 +0530

    added-display-info

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
@@ -11,7 +12,7 @@ const App = () => {
       <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-3">
         <Navbar />
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </div>
---
+++ src/components/DisplayInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayInfo = () => {
+  return <div className="text-white">DisplayInfo</div>;
+};
+
+export default DisplayInfo;
