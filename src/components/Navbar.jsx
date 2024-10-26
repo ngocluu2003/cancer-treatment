300dcd9b79f4407ba8acd07266c0aa873cd01bd3
code-walker-23 <chhabravinay549@gmail.com>
2024-10-26 14:24:44 +0530

    building-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useCallback } from "react";
-import { search } from "../assets";
+import React, { useCallback, useState } from "react";
+import { menu, search } from "../assets";
 import CustomButton from "./CustomButton";
 import { usePrivy } from "@privy-io/react-auth";
+import { IconHeartHandshake } from "@tabler/icons-react";
 const Navbar = () => {
   const { ready, authenticated, login, logout, user } = usePrivy();
+  const [toggleDrawer, setToggleDrawer] = useState(false);
   const handleLoginLogout = useCallback(() => {
     if (authenticated) {
       logout();
@@ -19,7 +21,7 @@ const Navbar = () => {
   return (
     <div className="mb-[35px] flex flex-col-reverse justify-between gap-6 md:flex-row">
       {/* search bar component */}
-      <div className="rounded-[100px] flex h-[52px] max-w-[458px] flex-row bg-[#1c1c24] py-2 pl-4 pr-2 lg:flex-1">
+      <div className="flex h-[52px] max-w-[458px] flex-row rounded-[100px] bg-[#1c1c24] py-2 pl-4 pr-2 lg:flex-1">
         <input
           type="text"
           placeholder="search for records"
@@ -40,6 +42,27 @@ const Navbar = () => {
           title={authenticated ? "Logout" : "Login"}
           handleClick={handleLoginLogout}
         />
+      </div>
+      {/* mobile version */}
+      <div className="relative flex items-center justify-between sm:hidden">
+        <div className="flex h-[14px] cursor-pointer items-center justify-center rounded-[10px] bg-[#2c2f32]">
+          <IconHeartHandshake size={14} color="#1ec070" className="p-2" />
+        </div>
+        <img
+          src={menu}
+          alt="menu"
+          className="h-[34px] w-[34px] cursor-pointer object-contain"
+          onClick={() => {
+            setToggleDrawer((prev) => !prev);
+          }}
+        />
+        <div
+          className={`absolute left-0 right-0 top-[60px] z-10 bg-[#1c1c24] py-4 shadow-secondary ${!toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"} transition-all duration-700`}
+        >
+          <ul className="mb-4 ">
+          {navLinks}
+          </ul>
+        </div>
       </div>
     </div>
   );
