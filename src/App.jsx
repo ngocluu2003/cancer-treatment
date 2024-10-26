fdfc5db8c7107710df2ed563ef42a9568ac2a353
code-walker-23 <chhabravinay549@gmail.com>
2024-10-26 22:35:43 +0530

    increased-duration

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Home } from "./pages";
 
 const App = () => {
   return (
-    <div className="relative flex min-h-screen flex-row bg-[#f5f5f5] dark:bg-[#13131a] p-4 transition-colors duration-200">
+    <div className="relative flex min-h-screen flex-row bg-[#f5f5f5] dark:bg-[#13131a] p-4 transition-colors duration-300">
       <div className="relative mr-10 hidden sm:flex">
         <Sidebar />
       </div>
