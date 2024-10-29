a25f3f17fefa4ede327de33ced365b4771e4b6db
code-walker-23 <chhabravinay549@gmail.com>
2024-10-29 15:39:36 +0530

    setup-post-and-get-requests

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -15,6 +15,40 @@ export const StateContextProvide = ({ children }) => {
     try {
       const results = await db.select().from(Users).execute();
       setUsers(results);
-    } catch (error) {}
-  });
+    } catch (error) {
+      console.error("Error fetching users", error);
+    }
+  }, []);
+
+  const fetchUserByEmail = useCallback(async (email) => {
+    try {
+      const result = await db
+        .select()
+        .from(users)
+        .where(eq(Users.createdBy, email));
+
+      if (result > 0) {
+        setCurrentUser(result[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching user by email", error);
+    }
+  }, []);
+
+  const createUser = useCallback(async (userData) => {
+    try {
+      const newUser = await db
+        .insert(Users)
+        .values(userData)
+        .returning()
+        .execute();
+      setUsers((prevUsers) => [...prevUsers, newUser[0]]);
+      return newUser[0];
+    } catch (error) {
+      console.error("Error creating user", error);
+      return null;
+    }
+  }, []);
+
+  const fetchUserRecords = useCallback(() => {}, []);
 };
