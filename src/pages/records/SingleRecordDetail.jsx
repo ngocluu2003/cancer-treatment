7ed56387d1aea725235f3aaad2c5f3be04e3c88b
code-walker-23 <chhabravinay549@gmail.com>
2024-10-31 00:08:18 +0530

    added-production-key

--- src/pages/records/SingleRecordDetail.jsx
+++ src/pages/records/SingleRecordDetail.jsx
@@ -1,7 +1,20 @@
+import { IconFileUpload } from "@tabler/icons-react";
 import React from "react";
 
 const SingleRecordDetail = () => {
-  return <div>SingleRecordDetail</div>;
+  return (
+    <div className="flex flex-wrap gap-[26px]">
+      <button
+        type="button"
+        onClick={() => {}}
+        className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
+      >
+        <IconFileUpload />
+        Upload Reports
+      </button>
+      {/* Upload Report Modal */}
+    </div>
+  );
 };
 
 export default SingleRecordDetail;
---
+++ src/pages/records/components/RecordDetailsHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecordDetailsHeader = ({ recordName }) => {
+  return <div>RecordDetailsHeader</div>;
+};
+
+export default RecordDetailsHeader;
