d477d500ac8378be82898f32f34ec38404deed18
code-walker-23 <chhabravinay549@gmail.com>
2024-10-31 01:15:14 +0530

    handling-_jsx-error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { babel } from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelConfig: path.resolve(__dirname, "babel.config.js"), // Ensure Vite uses your Babel config
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
