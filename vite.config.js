8299658e98da1fc059504c29f9f6c08f8c714e69
code-walker-23 <chhabravinay549@gmail.com>
2024-10-31 13:18:23 +0530

    setup-config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-=======
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
->>>>>>> 4d2f5fe6280b295b6d5dce33728bb4c6df7d641c
 
 export default defineConfig({
   plugins: [react()],
@@ -15,4 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  base: "./",
+});
