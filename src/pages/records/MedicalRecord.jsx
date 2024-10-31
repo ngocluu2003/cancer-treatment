1d9870f3ae249f706071234c285cf071d7add282
code-walker-23 <chhabravinay549@gmail.com>
2024-11-01 00:12:25 +0530

    handling-error

--- src/pages/records/MedicalRecord.jsx
+++ src/pages/records/MedicalRecord.jsx
@@ -73,15 +73,10 @@ const MedicalRecord = () => {
     navigate(`/medical-records/${name}`, { state: filteredRecords });
   };
 
-  // after the folder creation
-  if (contextLoading && !loading) {
-    return <div className="text-center text-[#1ec070]">Loading records...</div>;
-  }
-
   return (
     <div className="flex flex-wrap gap-[26px] bg-[#f5f5f5] dark:bg-[#13131a]">
       {/* Folder Creation Button */}
-      {!contextLoading && (
+      {true && (
         <button
           type="button"
           className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-300 bg-[#e9e9e9] px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-[#e3e3db] dark:border-neutral-700 dark:bg-[#1c1c24] dark:text-white dark:hover:bg-[#2c2f32]"
