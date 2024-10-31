1b3a005eb06585ad1c6ed837f1d7b51df1701f56
code-walker-23 <chhabravinay549@gmail.com>
2024-11-01 00:07:46 +0530

    handling-error

--- src/pages/records/MedicalRecord.jsx
+++ src/pages/records/MedicalRecord.jsx
@@ -81,15 +81,17 @@ const MedicalRecord = () => {
   return (
     <div className="flex flex-wrap gap-[26px] bg-[#f5f5f5] dark:bg-[#13131a]">
       {/* Folder Creation Button */}
-      <button
-        type="button"
-        className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-300 bg-[#e9e9e9] px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-[#e3e3db] dark:border-neutral-700 dark:bg-[#1c1c24] dark:text-white dark:hover:bg-[#2c2f32]"
-        onClick={handleOpenModal}
-        disabled={contextLoading}
-      >
-        <IconCirclePlus className="text-[#1ec070] dark:text-[#1dc071]" />
-        Create Record
-      </button>
+      {!contextLoading && (
+        <button
+          type="button"
+          className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-300 bg-[#e9e9e9] px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-[#e3e3db] dark:border-neutral-700 dark:bg-[#1c1c24] dark:text-white dark:hover:bg-[#2c2f32]"
+          onClick={handleOpenModal}
+        >
+          <IconCirclePlus className="text-[#1ec070] dark:text-[#1dc071]" />
+          Create Record
+        </button>
+      )}
+
       {/* Folder Creation Modal with Context Loading/Error as Props */}
       <CreateRecordModal
         isOpen={isModalOpen}
