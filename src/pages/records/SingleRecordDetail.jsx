60d5384c74a573f18a3d935bc7d0ed557fee4568
code-walker-23 <chhabravinay549@gmail.com>
2024-11-01 23:00:05 +0530

    building-gemini-feature

--- src/pages/records/SingleRecordDetail.jsx
+++ src/pages/records/SingleRecordDetail.jsx
@@ -8,6 +8,10 @@ import RecordDetailsHeader from "./components/RecordDetailsHeader";
 import { useLocation, useNavigate } from "react-router-dom";
 import FileUploadModal from "./components/FileUploadModal";
 import { useUserStateContext } from "../../context/UserContext";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const geminiApiKey = import.meta.env.VITE_GEMINI_API_KEY;
+
 const SingleRecordDetail = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
@@ -30,8 +34,9 @@ const SingleRecordDetail = () => {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  const handleFileChange = () => {
+  const handleFileChange = (e) => {
     const file = e.target.files[0];
+    console.log("file", file);
     setFileType(file.type);
     setFileName(file.name);
     setFile(file);
@@ -39,21 +44,79 @@ const SingleRecordDetail = () => {
   const readFileAsBase64 = (file) => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.onload = () => resolve(reader.result.split(".")[1]);
+      reader.onload = () => resolve(reader.result.split(",")[1]);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
     });
   };
+
+  const handleFileUpload = async () => {
+    setUploading(true);
+    setUploadSuccess(false);
+
+    const genAI = new GoogleGenerativeAI(geminiApiKey);
+
+    try {
+      const base64Data = await readFileAsBase64(file);
+
+      const imageParts = [
+        {
+          inlineData: {
+            data: base64Data,
+            mimeType: fileType,
+          },
+        },
+      ];
+
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+
+      const prompt = `You are an expert cancer and any disease diagnosis analyst. Use your knowledge base to answer questions about giving personalized recommended treatments.
+        give a detailed treatment plan for me, make it more readable, clear and easy to understand make it paragraphs to make it more readable
+        `;
+
+      const result = await model.generateContent([prompt, ...imageParts]);
+      const response = await result.response;
+      const text = response.text();
+      setAnalysisResult(text);
+      const updatedRecord = await updateRecord({
+        documentID: state.id,
+        analysisResult: text,
+        kanbanRecords: "",
+      });
+      setUploadSuccess(true);
+      setIsModalOpen(false); // Close the modal after a successful upload
+      setFileName("");
+      setFile(null);
+      setFileType("");
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      setUploadSuccess(false);
+      setIsModalOpen(false);
+    } finally {
+      setUploading(false);
+    }
+  };
+
   return (
     <div className="flex flex-wrap gap-[26px]">
       <button
         type="button"
-        onClick={() => {}}
+        onClick={handleOpenModal}
         className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
       >
         <IconFileUpload />
         Upload Reports
       </button>
       {/* Upload Report Modal */}
-      <FileUploadModal isOpen={isModalOpen} />
+      <FileUploadModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onFileChange={handleFileChange}
+        onFileUpload={handleFileUpload}
+        uploading={uploading}
+        uploadSuccess={uploadSuccess}
+        fileName={fileName}
+      />
       <RecordDetailsHeader recordName={state.recordName} />
       <div className="w-full">
         <div className="flex flex-col">
@@ -84,13 +147,12 @@ const SingleRecordDetail = () => {
                     className="inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800"
                   >
                     View Treatment Plan <IconChevronRight size={20} />
-                    {/* spinner */}
-                    {true && (
+                    {/* {processing && (
                       <IconProgress
-                        className="mr-3 h-5 w-5 animate-spin"
                         size={10}
+                        className="mr-3 h-5 w-5 animate-spin"
                       />
-                    )}
+                    )} */}
                   </button>
                 </div>
               </div>
--- src/pages/records/components/FileUploadModal.jsx
+++ src/pages/records/components/FileUploadModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "./Modal";
+import { IconProgress } from "@tabler/icons-react";
 const FileUploadModal = ({
   isOpen,
   onClose,
